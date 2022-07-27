346ed0fa847f801c000fa70d579334f431fe9a07
ahmed elrakhawy <elrakhawyahmed79@gmail.com>
2022-07-27 07:07:57 +0200

    Entities , Data Project Initialized , Adding Auction Entity ,Adding Entity Framework And Auctions Table Intialized

---
+++ DealDouble.Data/DealDoubleDBContext.cs
@@ -0,0 +1,18 @@
+﻿using DealDouble.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealDouble.Data
+{
+    public class DealDoubleDBContext : DbContext
+    {
+        public DealDoubleDBContext() : base("name=DealDoubleConnectionString")
+        {
+        }
+        public DbSet<Auction> Auctions { get; set; }
+    }
+}
